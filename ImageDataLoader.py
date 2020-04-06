8a5e6a723956105e19773a74fba698eab422b711
sihyun-yu <yusihyunc@kaist.ac.kr>
2020-04-06 19:13:40 +0900

    implemented fixmatch initial version

--- ImageDataLoader.py
+++ ImageDataLoader.py
@@ -16,6 +16,69 @@ class TransformTwice:
         out1 = self.transform(inp)
         out2 = self.transform(inp)
         return out1, out2    
+
+###################################################################################
+
+class FixMatchImageLoader(torch.utils.data.Dataset):
+    def __init__(self, args, split, transform=None, loader=default_image_loader, strong_transform=None):
+        rootdir = os.path.join(args.datadir)
+        self.impath = os.path.join(args.datadir, 'downimages')
+        self.datasets = []
+        if split == 'train' : data_file = os.path.join(rootdir, '%s' % args.trainfile)
+        elif split == 'test' : data_file = os.path.join(rootdir, '%s' % args.testfile)            
+        elif split == 'validation' : data_file = os.path.join(rootdir, '%s' % args.validfile)    
+        elif split == 'unlabel' : data_file = os.path.join(rootdir, '%s' % args.unlabelfile)   
+        else: print('wrong split information')
+            
+        meta_file = os.path.join(rootdir, data_file)    
+        self.subpath_to_idx_dict = {}
+        identity_class= set()
+        imnames = []
+        imclasses = []
+        classindex = []
+        
+        with open(meta_file, 'r') as rf:     
+            for idx, line in enumerate(rf):
+                if idx == 0:
+                    continue
+                instance_id, label, file_name = line.strip().split()        
+                category_id = int(label)
+                self.subpath_to_idx_dict[file_name] = len(self.datasets)
+                self.datasets.append( (os.path.join(self.impath, file_name), int(category_id)) )                    
+                imnames.append(file_name)
+                imclasses.append(int(label))
+                identity_class.add(int(label))
+
+        self.transform = transform
+        self.TransformTwice = TransformTwice(transform)
+        self.loader = loader
+        self.split = split
+        self.classnumber = len(identity_class)
+        self.imnames = imnames
+        self.imclasses = imclasses
+        self.strong_transform = strong_transform
+    
+    def __getitem__(self, index):
+        filename = self.imnames[index]
+        img_name = self.loader(os.path.join(self.impath, filename))
+        
+        if self.split != 'unlabel':
+            if self.transform is not None:
+                img = self.transform(img_name)
+            label = self.imclasses[index]            
+            return img, int(label)
+
+        else:        
+            img1, img2 = self.transform(img_name), self.strong_transform(img_name)
+            label = self.imclasses[index]  
+            return img1, img2, int(label)           
+            
+        
+    def __len__(self):
+        return len(self.imnames)               
+
+###################################################################################
+
     
 class SimpleImageLoader(torch.utils.data.Dataset):
     def __init__(self, args, split, transform=None, loader=default_image_loader):
