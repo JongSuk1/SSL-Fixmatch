e9464a79fb6d639e180b5acfb17acb4e69a0c4ac
sihyun-yu <yusihyunc@kaist.ac.kr>
2020-04-21 13:10:57 +0900

    finished classifier balancing

---
+++ balance_classifier.py
@@ -0,0 +1,464 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+import argparse
+
+import numpy as np
+import shutil
+import random
+import time
+import math
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim.lr_scheduler import LambdaLR
+from torch.autograd import Variable
+import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
+
+import torchvision
+from torchvision import datasets, models, transforms
+#from tensorboardX import SummaryWriter
+
+import torch.nn.functional as F
+
+from ImageDataLoader import SimpleImageLoader
+from models import Res18, Res50, Dense121, Res18_basic, weights_init_classifier
+from randaugment import RandAugmentMC
+
+import nsml
+from nsml import DATASET_PATH, IS_ON_NSML
+from crt import ClassAwareSampler
+
+NUM_CLASSES = 265
+if not IS_ON_NSML:
+    DATASET_PATH = '/workspace/cs492h-ssl/meta/'
+    
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        wrong_k = batch_size - correct_k
+        res.append(wrong_k.mul_(100.0 / batch_size))
+
+    return res
+
+def top_n_accuracy_score(y_true, y_prob, n=5, normalize=True):
+    num_obs, num_labels = y_prob.shape
+    idx = num_labels - n - 1
+    counter = 0
+    argsorted = np.argsort(y_prob, axis=1)
+    for i in range(num_obs):
+        if y_true[i] in argsorted[i, idx+1:]:
+            counter += 1
+    if normalize:
+        return counter * 1.0 / num_obs
+    else:
+        return counter
+        
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+        
+def adjust_learning_rate(opts, optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    """
+    Linear Warmup.
+    """
+    if epoch <= 5:
+        lr = opts.lr + (0.4-0.03) * (epoch - 1) / 4
+    elif epoch < 60:
+        lr = 0.4
+    elif epoch >= 60 and epoch < 120:
+        lr = 0.4 * 0.1
+    elif epoch >= 120 and epoch < 160:
+        lr = 0.4 * (0.1 ** 2)
+    elif epoch >= 160 and epoch < 200:
+        lr = 0.4 * (0.1 ** 3)
+    else:
+        lr = 0.4 * (0.1 ** 4)
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+        
+def linear_rampup(current, rampup_length):
+    if rampup_length == 0:
+        return 1.0
+    else:
+        current = np.clip(current / rampup_length, 0.0, 1.0)
+        return float(current)
+        
+class SemiLoss(object):
+    def __call__(self, outputs_x, targets_x, outputs_u, targets_u, epoch, final_epoch):
+        probs_u = torch.softmax(outputs_u, dim=1)
+        Lx = -torch.mean(torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1))
+        Lu = torch.mean((probs_u - targets_u)**2)
+        return Lx, Lu, opts.lambda_u * linear_rampup(epoch, final_epoch)
+
+def interleave_offsets(batch, nu):
+    groups = [batch // (nu + 1)] * (nu + 1)
+    for x in range(batch - sum(groups)):
+        groups[-x - 1] += 1
+    offsets = [0]
+    for g in groups:
+        offsets.append(offsets[-1] + g)
+    assert offsets[-1] == batch
+    return offsets
+
+def interleave(xy, batch):
+    nu = len(xy) - 1
+    offsets = interleave_offsets(batch, nu)
+    xy = [[v[offsets[p]:offsets[p + 1]] for p in range(nu + 1)] for v in xy]
+    for i in range(1, nu + 1):
+        xy[0][i], xy[i][i] = xy[i][i], xy[0][i]
+    return [torch.cat(v, dim=0) for v in xy]
+
+
+def split_ids(path, ratio):
+    with open(path) as f:
+        ids_l = []
+        ids_u = []
+        for i, line in enumerate(f.readlines()):
+            if i == 0 or line == '' or line == '\n':
+                continue
+            line = line.replace('\n', '').split('\t')
+            if int(line[1]) >= 0:
+                ids_l.append(int(line[0]))
+            else:
+                ids_u.append(int(line[0]))
+
+    ids_l = np.array(ids_l)
+    ids_u = np.array(ids_u)
+
+    perm = np.random.permutation(np.arange(len(ids_l)))
+    cut = int(ratio*len(ids_l))
+    train_ids = ids_l[perm][cut:]
+    val_ids = ids_l[perm][:cut]
+
+    return train_ids, val_ids, ids_u
+
+
+### NSML functions
+def _infer(model, root_path, test_loader=None):
+    if test_loader is None:
+        test_loader = torch.utils.data.DataLoader(
+            SimpleImageLoader(root_path, 'test',
+                               transform=transforms.Compose([
+                                   transforms.Resize(opts.imResize),
+                                   transforms.CenterCrop(opts.imsize),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                               ])), batch_size=opts.batchsize, shuffle=False, num_workers=4, pin_memory=True)
+        print('loaded {} test images'.format(len(test_loader.dataset)))
+
+    outputs = []
+    s_t = time.time()
+    for idx, image in enumerate(test_loader):
+        if torch.cuda.is_available():
+            image = image.cuda()
+        _, probs = model(image)
+        output = torch.argmax(probs, dim=1)
+        output = output.detach().cpu().numpy()
+        outputs.append(output)
+
+    outputs = np.concatenate(outputs)
+    return outputs
+
+def bind_nsml(model):
+    def save(dir_name, *args, **kwargs):
+        os.makedirs(dir_name, exist_ok=True)
+        state = model.state_dict()
+        torch.save(state, os.path.join(dir_name, 'model.pt'))
+        print('saved')
+
+    def load(dir_name, *args, **kwargs):
+        state = torch.load(os.path.join(dir_name, 'model.pt'))
+        model.load_state_dict(state)
+        print('loaded')
+
+    def infer(root_path):
+        return _infer(model, root_path)
+
+    nsml.bind(save=save, load=load, infer=infer)
+
+######################################################################
+# Options
+######################################################################
+parser = argparse.ArgumentParser(description='Sample Product200K Training')
+parser.add_argument('--start_epoch', type=int, default=1, metavar='N', help='number of start epoch (default: 1)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 200)')
+
+# basic settings
+parser.add_argument('--name',default='HA_trial3', type=str, help='output model name')
+parser.add_argument('--gpu_ids',default='0', type=str,help='gpu_ids: e.g. 0  0,1,2  0,2')
+parser.add_argument('--batchsize', default=256, type=int, help='batchsize')
+parser.add_argument('--seed', type=int, default=123, help='random seed')
+
+# basic hyper-parameters
+parser.add_argument('--momentum', type=float, default=0.9, metavar='LR', help=' ')
+parser.add_argument('--lr', type=float, default=0.03, metavar='LR', help='learning rate (default: 5e-5)')
+parser.add_argument('--imResize', default=256, type=int, help='')
+parser.add_argument('--imsize', default=224, type=int, help='')
+parser.add_argument('--lossXent', type=float, default=1, help='lossWeight for Xent')
+
+# arguments for logging and backup
+parser.add_argument('--log_interval', type=int, default=10, metavar='N', help='logging training status')
+parser.add_argument('--save_epoch', type=int, default=50, help='saving epoch interval')
+
+# hyper-parameters for fixmatch
+parser.add_argument('--lambda-u', default=1, type=float)
+parser.add_argument('--mu', default=1 , type=int, help="Batch ratio between labeled/unlabeled")
+parser.add_argument('--threshold', type=float, default=0.95, help='Threshold setting for Fixmatch')
+
+
+parser.add_argument('--T', default=0.5, type=float)
+parser.add_argument('--val-iteration', type=int, default=100, help='Number of labeled data')
+
+### DO NOT MODIFY THIS BLOCK ###
+# arguments for nsml 
+parser.add_argument('--pause', type=int, default=0)
+parser.add_argument('--mode', type=str, default='train')
+parser.add_argument('--pretrained', type=str, default="")
+################################
+
+def main():
+    global opts
+    opts = parser.parse_args()
+    opts.cuda = 0
+
+#    global writer
+#    writer = SummaryWriter("runs/"+opts.name)
+    # Set GPU
+    seed = opts.seed
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = opts.gpu_ids
+    use_gpu = torch.cuda.is_available()
+    if use_gpu:
+        opts.cuda = 1
+        print("Currently using GPU {}".format(opts.gpu_ids))
+        cudnn.benchmark = True
+        torch.cuda.manual_seed_all(seed)
+    else:
+        print("Currently using CPU (GPU is highly recommended)")
+
+
+    # Set model
+    model = Res50(NUM_CLASSES)
+    if not IS_ON_NSML:
+        model = torch.nn.DataParallel(model)    
+
+    parameters = filter(lambda p: p.requires_grad, model.parameters())
+    n_parameters = sum([p.data.nelement() for p in model.parameters()])
+    print('  + Number of params: {}'.format(n_parameters))
+
+    if use_gpu:
+        model.cuda()
+
+
+    ### DO NOT MODIFY THIS BLOCK ###
+    if IS_ON_NSML:
+        bind_nsml(model)
+        if opts.pause:
+            nsml.paused(scope=locals())
+    ################################
+
+
+    ### Load the model
+    if IS_ON_NSML:
+        checkpoint = torch.load(opts.pretrained)
+        nsml.load()
+    else:
+        checkpoint = torch.load("./runs/HA_trial3_e299.pth.tar")
+        model.load_state_dict(checkpoint)
+        model.module.classifier.apply(weights_init_classifier)
+    
+    
+    if opts.mode == 'train':
+        model.train()
+        # Set dataloader
+        train_ids, val_ids, unl_ids = split_ids(os.path.join(DATASET_PATH, 'train/train_label'), 0.2)
+        print('found {} train, {} validation and {} unlabeled images'.format(len(train_ids), len(val_ids), len(unl_ids)))
+        
+        #Sampler
+        crtSampler = ClassAwareSampler(data_source=DATASET_PATH + '/train/', ids=train_ids)
+
+        train_loader = torch.utils.data.DataLoader(
+            SimpleImageLoader(DATASET_PATH, 'train', train_ids,
+                              transform=transforms.Compose([
+                                  transforms.Resize(opts.imResize),
+                                  transforms.RandomResizedCrop(opts.imsize),
+                                  transforms.RandomHorizontalFlip(),
+                                  transforms.RandomVerticalFlip(),
+                                  transforms.ToTensor(),
+                                  transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])),
+                                batch_size=opts.batchsize, num_workers=4, pin_memory=True, drop_last=True,
+                                sampler=crtSampler)
+        print('train_loader done')
+
+        validation_loader = torch.utils.data.DataLoader(
+            SimpleImageLoader(DATASET_PATH, 'val', val_ids,
+                               transform=transforms.Compose([
+                                   transforms.Resize(opts.imResize),
+                                   transforms.CenterCrop(opts.imsize),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])),
+                               batch_size=opts.batchsize, shuffle=False, num_workers=4, pin_memory=True, drop_last=False)
+        print('validation_loader done')
+
+        # Set optimizer
+        #optimizer = optim.Adam(model.parameters(), lr=opts.lr)
+        optimizer = optim.SGD(model.parameters(), lr=opts.lr,  momentum=opts.momentum, nesterov=True, weight_decay=0.0001)
+        
+        criterion = nn.CrossEntropyLoss().cuda()
+        best_acc = 0.0
+        
+        for epoch in range(opts.start_epoch, opts.epochs + 1):
+            print('start training')
+            #adjust_learning_rate(opts, optimizer, epoch)
+            loss, train_top1, train_top5 = train(opts, train_loader, model, criterion, optimizer, epoch, use_gpu)
+            acc_top1, acc_top5 = validation(opts, validation_loader, model, epoch, use_gpu)
+            
+            is_best = acc_top1 > best_acc
+            best_acc = max(acc_top1, best_acc)
+            if is_best:
+                print('saving best checkpoint...')
+                if IS_ON_NSML:
+                    nsml.save(opts.name + '_best')
+                else:
+                    torch.save(model.state_dict(), os.path.join('runs', opts.name + '_best.pth.tar'))
+            if (epoch + 1) % 2 == 0:
+                if IS_ON_NSML:
+                    nsml.save(opts.name + '_e{}'.format(epoch))
+                else:
+                    torch.save(model.state_dict(), os.path.join('runs', opts.name + '_e{}.pth.tar'.format(epoch)))
+                
+
+def train(opts, train_loader, model, criterion, optimizer, epoch, use_gpu):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    for param in model.parameters():
+        param.requires_grad = False
+    # Except for fc
+    if IS_ON_NSML:
+        for param in model.classifier.parameters():
+            param.requires_grad = True
+    else:
+        for param in model.module.classifier.parameters():
+            param.requires_grad = True
+        
+    end = time.time()
+    for i, (input, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        input = input.cuda()
+        target = target.cuda()
+
+        # compute output
+        input_var = torch.autograd.Variable(input, requires_grad=True)
+        target_var = torch.autograd.Variable(target)
+        _, output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # measure accuracy and record loss
+        err1, err5 = accuracy(output.data, target, topk=(1, 5))
+
+        losses.update(loss.item(), input.size(0))
+        top1.update(err1.item(), input.size(0))
+        top5.update(err5.item(), input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % 10 == 0:
+            print('Epoch: [{0}/{1}][{2}/{3}]\t'
+                  'LR: {LR:.6f}\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Top 1-acc {top1.val:.4f} ({top1.avg:.4f})\t'
+                  'Top 5-acc {top5.val:.4f} ({top5.avg:.4f})'.format(
+                epoch, opts.epochs, i, len(train_loader), LR=opts.lr, batch_time=batch_time,
+                data_time=data_time, loss=losses, top1=top1, top5=top5))
+
+    print('* Epoch: [{0}/{1}]\t Top 1-acc {top1.avg:.3f}  Top 5-acc {top5.avg:.3f}\t Train Loss {loss.avg:.3f}'.format(
+        epoch, opts.epochs, top1=top1, top5=top5, loss=losses))
+
+    return losses.avg, top1.avg, top5.avg
+
+def validation(opts, validation_loader, model, epoch, use_gpu):
+    model.eval()
+    avg_top1= 0.0
+    avg_top5 = 0.0
+    nCnt =0
+    steps = (epoch - 1) * len(validation_loader)
+
+    with torch.no_grad():
+        for batch_idx, data in enumerate(validation_loader):
+            inputs, labels = data
+            if use_gpu :
+                inputs = inputs.cuda()
+            inputs = Variable(inputs)
+            steps+=1
+            nCnt +=1
+            embed_fea, preds = model(inputs)
+
+            acc_top1 = top_n_accuracy_score(labels.numpy(), preds.data.cpu().numpy(), n=1)*100
+            acc_top5 = top_n_accuracy_score(labels.numpy(), preds.data.cpu().numpy(), n=5)*100
+            avg_top1 += acc_top1
+            avg_top5 += acc_top5
+
+#            if not IS_ON_NSML:
+#                writer.add_scalar('test_step/acc_val_top1', acc_top1, steps)    
+#                writer.add_scalar('test_step/acc_val_top5', acc_top5, steps)
+        avg_top1 = float(avg_top1/nCnt)   
+        avg_top5= float(avg_top5/nCnt)   
+        print('Test Epoch:{} Top1_acc_val:{:.2f}% Top5_acc_val:{:.2f}% '.format(epoch, avg_top1, avg_top5))
+    return avg_top1, avg_top5
+
+
+
+if __name__ == '__main__':
+    main()
+
+
